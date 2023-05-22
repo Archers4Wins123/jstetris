d2b564cbf629f00357e16f886c8f67e01035d918
Wenpan Wu <wuwenpan@hotmail.com>
2023-05-22 14:54:37 -0600

    timer created

--- src/components/GridBoard.js
+++ src/components/GridBoard.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import GridSquare from "./GridSquare";
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import {shapes} from '../utils'
+import { moveDown } from "../actions";
 
 export default function GridBoard(props) {
+    const requestRef = useRef()
+    const lastUpdateTimeRef = useRef(0)
+    const progressTimeRef = useRef(0)
+    const dispatch = useDispatch()
     const game = useSelector((state) => state.game)
     const {grid, shape, rotation, x, y, isRunning, speed } = game
 
     const block = shapes[shape][rotation]
-const blockColor = shape
-const gridSquares = grid.map((rowArray, row) => {
+    const blockColor = shape
+    const gridSquares = grid.map((rowArray, row) => {
     return rowArray.map((square, col) => {
         const blockX = col - x
         const blockY = row - y
@@ -21,22 +26,33 @@ const gridSquares = grid.map((rowArray, row) => {
         return <GridSquare
                 key={k}
                 color={color} />
+        })
     })
-})
-return (
-    <div className='grid-board'>
-        {gridSquares}
-    </div>
-)
-    /*for (let row = 0; row < 18; row++) {
-        grid.push([])
-        for (let col = 0; col < 10; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color="1" />)
+    const update = (time) => {
+        requestRef.current = requestAnimationFrame(update)
+        if (!isRunning) {
+            return
         }
+        if(!lastUpdateTimeRef.current) {
+            lastUpdateTimeRef.current = time;
+        }
+        const deltaTime = time - lastUpdateTimeRef.current;
+        progressTimeRef.current += deltaTime
+        if (progressTimeRef.current > speed) {
+            dispatch(moveDown());
+            progressTimeRef.current = 0;
+        }
+        lastUpdateTimeRef.current = time
     }
+    
+    useEffect(() => {
+        requestRef.current = requestAnimationFrame(update)
+        return () => cancelAnimationFrame(requestRef.current)
+    }, [isRunning])
+
     return (
         <div className='grid-board'>
-            {grid}
+            {gridSquares}
         </div>
-    )*/
+    )
 }
